98036f4908c05ed6a84d9f7b640f614ed6678659
Maxim Obrubov <obrubov.max@mail.ru>
2017-11-21 22:11:03 +0300

    Создана БД и модель данных EF Database First

--- OnlineShop/OnlineShop/Controllers/HomeController.cs
+++ OnlineShop/OnlineShop/Controllers/HomeController.cs
@@ -17,32 +17,14 @@ namespace OnlineShop.Controllers
 
         public ActionResult About()
         {
-            //ViewBag.Message = "Your application description page.";
-            ApplicationEntitiesDbContext dbContext = ApplicationEntitiesDbContext.Create();
-            dbContext.Addresses.Add(new Address
-            {
-                Postcode = "12312312",
-                Country = "12312312",
-                Region = "12312312",
-                City = "12312312",
-                Street = "12312312",
-                House = "12312312",
-                Flat = "12312312"
-            });
-            dbContext.SaveChanges();
+            ViewBag.Message = "Your application description page.";
+            
             return View();
         }
 
         public ActionResult Contact()
         {
-            ApplicationEntitiesDbContext dbContext = ApplicationEntitiesDbContext.Create();
-            string text = "";
-            var users = dbContext.Users;
-            foreach(var user in users)
-            {
-                text += user.Name + '\n';
-            }
-            ViewBag.Message = text;
+            ViewBag.Message = "Your application contacts page.";
 
             return View();
         }
